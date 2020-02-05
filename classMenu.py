6dec7546b0ab08cc74a0825e1e9b08f80e4b257e
Marilyn-stack <57353265+Marilyn-stack@users.noreply.github.com>
2020-02-05 15:46:41 +0100

    ANTES DE JODERLA DANIEL

--- classMenu.py
+++ classMenu.py
@@ -7,7 +7,8 @@ class Menu:
         print("Elige la opción" , "\n")
         print("\t1 - Guardar")
         print("\t2 - Consultar")
-        print("\t3 - tercera opción")
+        print("\t3 - Eliminar")
+        print("\t4 - Buscar")
         print("\t9 - salir" , "\n")
     def menu2(self):
         while True:
@@ -19,8 +20,11 @@ class Menu:
                 self.libro1.consultar() 
                 input("Has pulsado la opción 2...\npulsa una tecla para continuar")
             elif opcionMenu == "3":
-                #libro1.limpiar()
+                self.libro1.eliminar()
                 input("Has pulsado la opción 3...\npulsa una tecla para continuar")
+            elif opcionMenu == "4":
+                self.libro1.buscar()
+                input("Has pulsado la opción 4...\npulsa una tecla para continuar")
             elif opcionMenu == "9":
                 break
             else:
